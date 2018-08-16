a13271830e0e51b723b0b5fa2743ad018f802b82
DESKTOP-NKGR7AM\User <manyak.yuriy@gmail.com>
2018-08-16 10:08:55 +0300

    Create the scheduler as a producer-consumer wrapper

---
+++ SyncTaskScheduler.Contracts/Scheduler/ITaskScheduler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTaskScheduler.Contracts.Scheduler
+{
+    public interface ITaskScheduler<TPipeLineItem>
+    {
+        
+    }
+}
---
+++ SyncTaskScheduler.Implementation/Scheduler/TaskScheduler.cs
@@ -0,0 +1,19 @@
+﻿using SyncTaskScheduler.Contracts.PipeLine;
+using SyncTaskScheduler.Contracts.Scheduler;
+
+namespace SyncTaskScheduler.Implementation.Scheduler
+{
+    public class TaskScheduler<TPipeLineItem> : ITaskScheduler<TPipeLineItem>
+    {
+        private readonly IPipeLineProducer<TPipeLineItem> _pipeLineProducer;
+        private readonly IPipeLineConsumer<TPipeLineItem> _pipeLineConsumer;
+
+        public TaskScheduler(IPipeLineProducer<TPipeLineItem> pipeLineProducer, IPipeLineConsumer<TPipeLineItem> pipeLineConsumer)
+        {
+            _pipeLineProducer = pipeLineProducer;
+            _pipeLineConsumer = pipeLineConsumer;
+        }
+
+
+    }
+}
